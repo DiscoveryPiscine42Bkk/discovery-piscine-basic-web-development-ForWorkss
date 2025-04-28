2a6666d3163ada35245622610e4000f248e520e3
Thanaphat Chongkananurak <thachong@z2t4c3.42bangkok.com>
2025-04-28 17:57:57 +0700

    add the web and ex04 from cell0

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,11 +1,9 @@
-echo "$1"
-
-echo "$2"
-
-echo "$3"
-
-if  [$# > 3 | $# == 0 ]; then
-    echo "Invalid input, make sure it is more than 1 or less than 3 input"
-    exit 1
-
+if [ $# -eq 0 ]; then
+    echo "must provide at least 1"
+elif [ $# -eq 3 ]; then
+    echo "$@"
+elif [ $# -lt 3 ]; then
+    echo "$@"
+else
+    :
 fi
